5d6b589b6b4c2af4febecc2559513c3819302da5
Ric <ricardomarsanc@gmail.com>
2024-05-18 15:37:57 +0200

    feat: support for function components

--- src/index.jsx
+++ src/index.jsx
@@ -111,18 +111,31 @@ function commitWork(fiber) {
     return
   }
 
-  const domParent = fiber.parent.dom
+  let domParentFiber = fiber.parent
+  while (!domParentFiber.dom) {
+    domParentFiber = domParentFiber.parent
+  }
+
+  const domParent = domParentFiber.dom
 
   if (fiber.effectTag === "PLACEMENT" && fiber.dom !== null) {
     domParent.appendChild(fiber.dom)
   } else if (fiber.effectTag === "DELETION") {
-    domParent.removeChild(fiber.dom)
+    commitDeletion(fiber, domParent)
   } else if (fiber.effectTag === "UPDATE" && fiber.dom !== null) {
     updateDom(fiber.dom, fiber.alternate.props, fiber.props)
   }
 
   commitWork(fiber.child)
   commitWork(fiber.sibling)
+}
+
+function commitDeletion(fiber, domParent) {
+  if (fiber.dom) {
+    domParent.removeChild(fiber.dom)
+  } else {
+    commitDeletion(fiber.child, domParent)
+  }
 }
 
 function render(element, container) {
@@ -170,12 +183,13 @@ requestIdleCallback(workLoop)
  * @returns The next unit of work (fiber) to be performed.
  */
 function performUnitOfWork(fiber) {
-  if (!fiber.dom) {
-    fiber.dom = createDom(fiber)
-  }
+  const isFunctionComponent = fiber.type instanceof Function
 
-  const elements = fiber.props.children
-  reconcileChildren(fiber, elements)
+  if (isFunctionComponent) {
+    updateFunctionComponent(fiber)
+  } else {
+    updateHostComponent(fiber)
+  }
 
   if (fiber.child) {
     return fiber.child
@@ -188,6 +202,20 @@ function performUnitOfWork(fiber) {
     }
     nextFiber = nextFiber.parent
   }
+}
+
+function updateFunctionComponent(fiber) {
+  // type property of the fiber object is a function in case of a function component
+  const children = [fiber.type(fiber.props)]
+  reconcileChildren(fiber, children)
+}
+
+function updateHostComponent(fiber) {
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber)
+  }
+
+  reconcileChildren(fiber, fiber.props.children)
 }
 
 function reconcileChildren(wipFiber, elements) {
@@ -251,20 +279,11 @@ const RReact = {
 // RReact App
 
 /** @jsx RReact.createElement */
+function App(props) {
+  return <h1>Hi, {props.name}</h1>
+}
+
+const element = <App name="Ric" />
 const container = document.getElementById("root")
 
-const updateValue = (e) => {
-  rerender(e.target.value)
-}
-
-const rerender = (value) => {
-  const element = (
-    <div>
-      <input onInput={updateValue} value={value} />
-      <h2>Hello {value}</h2>
-    </div>
-  )
-  RReact.render(element, container)
-}
-
-rerender("World")
+RReact.render(element, container)
